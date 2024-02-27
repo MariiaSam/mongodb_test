cc213a5995e9dfed891e6b4f8fb060967456306c
MariiaSam <kontragent@ukr.net>
2024-02-27 09:38:10 +0200

    auth

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ import express from "express";
 
 import bookRoutes from './books.js'
 
+import authRouter from './auth'
+
 const router = express.Router()
 
 router.use('/books', bookRoutes)  
+router.use('/auth', authRouter)  
 
 export default router  
+
+
