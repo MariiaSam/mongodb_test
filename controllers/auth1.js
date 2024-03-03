0a618938c0db15ea559f74b64b3365fd92ba2137
MariiaSam <kontragent@ukr.net>
2024-03-03 20:03:00 +0200

    add

--- controllers/auth1.js
+++ controllers/auth1.js
@@ -1,30 +1,30 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 
-// import { User } from "../models/users.js";
-// import wrapper from "../helpers/wrapper.js";
-// import HttpError from "../helpers/HttpError.js";
+import { User } from "../models/users.js";
+import wrapper from "../helpers/wrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
 
-// const register = async(req, res) => {
-//     const { email } = req.body
-//     const user = await User.findOne({email})
+const register = async(req, res) => {
+    const { email } = req.body
+    const user = await User.findOne({email})
 
-//     if(user){
-//         throw HttpError(409, "Email already in use");
-//     }
+    if(user){
+        throw HttpError(409, "Email already in use");
+    }
 
-//     const newUser = await User.create(req.body)
+    const newUser = await User.create(req.body)
 
-//     res.status(201).json({
-//         email: newUser.email,
-//         name: newUser.name,
-//     })
+    res.status(201).json({
+        email: newUser.email,
+        name: newUser.name,
+    })
  
  
-// }
+}
 
-// export default {
-//     register: wrapper(register),
+export default {
+    register: wrapper(register),
   
-//   } 
+  } 
